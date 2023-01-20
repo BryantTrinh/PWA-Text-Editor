214dc54c2e8617f0d1236093c11775455315b54e
Bryant Trinh <trinhbryant@gmail.com>
2023-01-19 17:13:00 -0800

    finished database.js

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -14,7 +14,7 @@ const initdb = async () =>
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
 export const putDb = async (content) => {
-  console.log('PUT to the database');
+  console.log('Put- Added to the database');
   const jateDb = await.openDB('jate', 1);
   const tx = jateDb.transaction('jate', 'readwrite');
   const store = tx.objectStore('jate');
@@ -25,7 +25,20 @@ export const putDb = async (content) => {
 
 // TODO: Add logic for a method that gets all the content from the database
 export const getDb = async () => {
-  
-}
+console.log('Get-Retrieved from the database');
+  const jateDb = await.openDB('jate', 1);
+  const tx = jateDb.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  const request = store.get( 1 );
+  const result = await request;
+  if (result) {
+    console.log('Data has been retrieved from the JATE database', result.value);
+    return result.value;
+  } else {
+    console.log('Data has not been found from the JATE database');
+    return undefined;
+  };
+};
+
 
 initdb();
